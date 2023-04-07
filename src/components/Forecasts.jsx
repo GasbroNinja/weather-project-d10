519b896b2b93e571c25ae654462e231e64c75019
GasbroNinja <gasbroider@live.it>
2023-04-07 17:12:06 +0200

    Project weather App without forecasts on next Days

---
+++ src/components/Forecasts.jsx
@@ -0,0 +1,34 @@
+/* DA COMPLETARE
+
+import { useState } from "react";
+import { Container, Row, Col, Form, Spinner, Alert } from "react-bootstrap";
+
+
+const BASE_URL_2 = "https://api.openweathermap.org/data/2.5/forecast?q=";
+
+const Forecasts = () => {
+  //    const location = useLocation();
+
+  const [query, setQuery] = useState("");
+  const [country, setCountry] = useState("");
+  const [city, setCity] = useState("");
+  const [mainTemp, setMainTemp] = useState("");
+  const [description, setDescription] = useState("");
+  const [main, setMain] = useState("");
+  const [pressure, setPressure] = useState("");
+  const [humidity, setHumidity] = useState("");
+  const [bgGif, setBgGif] = useState("");
+  const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState(false);
+  const [errorMsg, setErrorMsg] = useState("");
+
+  return (
+  
+  <>
+  
+  
+  
+  </>
+  );
+};
+*/
